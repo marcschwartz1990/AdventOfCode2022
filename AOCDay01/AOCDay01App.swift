fe2183f25da6359662b7ef82c6f35e51ef6083f2
Marc Schwartz <marcschwartz1990@gmail.com>
2022-12-01 12:08:28 -0500

    Initial Commit

---
+++ AOCDay01/AOCDay01App.swift
@@ -0,0 +1,17 @@
+//
+//  AOCDay01App.swift
+//  AOCDay01
+//
+//  Created by Marc-Developer on 12/1/22.
+//
+
+import SwiftUI
+
+@main
+struct AOCDay01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
